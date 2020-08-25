b5f548100c838c6cd12d8765f36bef26acecde4b
사용자 <@DESK>
2020-08-25 11:50:30 +0900

    d

---
+++ lifeinfo/src/main/java/kr/co/mseshop/agent/ShinClass.java
@@ -0,0 +1,20 @@
+package kr.co.mseshop.agent;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShinClass {
+
+	String destValue;
+
+	public String getDestValue() {
+		return destValue;
+	}
+
+	public void setDestValue(String destValue) {
+		this.destValue = destValue;
+	}
+	
+	
+	
+}
