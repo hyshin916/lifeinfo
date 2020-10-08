10afa425da8a56056fcb63eb55c06d350b70587f
사용자 <@DESK>
2020-10-09 05:55:37 +0900

    a

---
+++ lifeinfo/src/main/java/kr/co/mseshop/exceptions/BaseExceptionResolver.java
@@ -0,0 +1,34 @@
+package kr.co.mseshop.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+public class BaseExceptionResolver implements HandlerExceptionResolver {
+
+	
+	Logger logger = Logger.getLogger(this.getClass());
+	
+	private String view;
+	
+	public void setView(String view) {
+		this.view = view;
+	}
+	
+	@Override
+	public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler,
+			Exception ex) {
+//		System.out.println("################### Exception Resolver ###################");
+//		System.out.println(ex.getMessage());
+//		ex.printStackTrace();
+		
+		logger.error("[ERROR Message]" + ex.getMessage());
+		logger.error("[System msg]" + ex);
+	
+		return new ModelAndView(view,"Exception",ex);
+	}
+
+}
---
+++ lifeinfo/src/main/webapp/WEB-INF/views/error/error.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+<h2>죄송합니다.<br>요청하신 페이지를 찾을 수 없습니다.</h2>
+<div class="content">
+<p>방문하시려는 페이지의 주소가 잘못 입력되었거나,<br>페이지의 주소가 변경 혹은 삭제되어 요청하신 페이지를 찾을 수 없습니다.</p>
+<p>입력하신 주소가 정확한지 다시 한번 확인해 주시기 바랍니다.</p>
+<p>감사합니다.</p>
+</div>
+</body>
+</html>
