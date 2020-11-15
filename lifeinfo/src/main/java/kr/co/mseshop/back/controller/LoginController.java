3b49f0330936b49b16e785ad075edbe4ede17c18
hyshin916 <hyshin916@gmail.com>
2020-11-15 12:03:08 +0900

    1203

--- lifeinfo/src/main/java/kr/co/mseshop/back/controller/LoginController.java
+++ lifeinfo/src/main/java/kr/co/mseshop/back/controller/LoginController.java
@@ -74,8 +74,7 @@ public class LoginController {
 		ck.setPath("/");
 		response.addCookie(ck);
 		
-		
-		System.out.println("aaaaaaaaa");
+		System.out.println("dadfsdf");
 		
 		int isLoginChk = loginService.getLogin(user_id, user_passwd);
 		JSONObject jObj = new JSONObject();
